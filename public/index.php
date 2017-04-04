cf370183e819b9a367d175c0b09ab890852cec63
Max_Biryukov <max_remmash@mail.ru>
2017-04-04 20:42:16 +0300

    правки

--- public/index.php
+++ public/index.php
@@ -2,12 +2,12 @@
 
     require __DIR__.'/../autoload.php';
     echo "<pre>";
-    
+
     $html = Classes\Data\Data::getHtmlByFile( 'data.txt' )
                               ->parseHtmlTags()
                               ->get()
                               ;
-    
+
     var_dump( $html );
 
 
@@ -15,26 +15,26 @@
                               ->parseHrefs()
                               ->getAsHtml()
                               ;
-                              
+
     var_dump( $html );
-  
+
     /*
     Методы парсинга:
         parseHtmlTags() - ищет все теги и их количество
         parseHrefs() - ищет все ссылки
         parsePictures() - ищет все картинки
-    
+
     Методы отображения результата:
         get() - вывод массива
         getAsHtml() - вывод html
         getAsXml() - вывод xml
-    
-    
-    Classes\Data\Data::getHtmlByUrl( 'http://move.ru' )
+
+
+    Classes\Data\Data::getHtmlByUrl( 'http://yandex.ru' )
                         ->parsePictures()
                         ->getAsXml()
-    
-    
+
+
     */
-    
+
 ?>
